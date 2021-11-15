d4aab3354756432b709d8c1099888f390156aad5
Ricky Thang <rthang11@yahoo.com>
2021-11-15 13:57:25 -0800

    Login page created

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const { csrfProtection } = require('./utils');
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', csrfProtection, function(req, res, next) {
+  res.render('login', { csrfToken: req.csrfToken() });
+});
+
+router.get('/signup', function(req, res, next) {
+  res.send('respond with a signup page');
 });
 
 module.exports = router;
