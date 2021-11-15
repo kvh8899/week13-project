761d4afe77a2df27b79270091410930fc852f910
Justin Russo <j.russo@outlook.com>
2021-11-15 18:15:20 -0500

    remove /users from usersRouter

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(
 store.sync();
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
