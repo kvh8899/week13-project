4e1438be9667ea1fb303363a13d2642cc19f68ba
Kyle <kylevhuang@gmail.com>
2021-11-16 15:28:21 -0800

    more home page styling

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,28 @@
+
+
 document.addEventListener("DOMContentLoaded",() => {
+    const background = document.querySelector('#startWriting');
+    const topbar = document.querySelector('.topbar');
+    const login = document.querySelector('.login');
+    const observer = new IntersectionObserver((entries,observer) => {
+        entries.forEach(entry => {
+            if(entry.isIntersecting === false) {
+                topbar.classList.remove("topbarB");
+                topbar.classList.add("topbarA");
+                login.classList.remove("loginA");
+                login.classList.add("loginB");
+                
+            }else{
+                topbar.classList.add("topbarB");
+                topbar.classList.remove("topbarA");
+                login.classList.add("loginA");
+                login.classList.remove("loginB");
+            }
+        })
+    },{
+        threshold:0.25
+    })
     
+    observer.observe(background);
 })
+console.log('observer')
