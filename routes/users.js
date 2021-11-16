9771059b0d181594e151cb4c90b88530a7dbc67c
kenjulian <kenneth.julian@yahoo.com>
2021-11-15 19:32:11 -0500

    validations

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,70 @@
 const bcrypt = require('bcryptjs');
 const express = require('express');
+const { check } = require('express-validator');
+const { Op } = require('sequelize');
 
 const { loginUser, restoreUser } = require('../auth');
 const { User } = require('../db/models')
 const { asyncHandler, csrfProtection } = require('./utils');
 
 const router = express.Router();
+
+const validateEmailAndPass = [
+  check('email')
+  .exists({checkFalsey: true})
+  .withMessage('Please provide an email address.')
+  .isLength({max: 255})
+  .withMessage('Email address must not be more than 255 characters long.')
+  .isEmail().withMessage('Email address is invalid.'),
+
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be more than 8 characters long")
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/, "g")
+    .withMessage(
+      'Password must contain at least 1 lowercase letter, uppercase letter, number, and special character (i.e. "!@#$%^&*")'
+    ),
+   
+];
+
+const signupValidator = [
+  check('username')
+  .exists({checkFalsey: true})
+  .withMessage('Please provide an username.')
+  .isLength({max: 50})
+  .withMessage('Username must be less than or equal to 50 characters.'),
+
+  check("confirmPassword")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a value for Confirm Password")
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Confirm Password does not match Password");
+      }
+      return true;
+    }),
+
+  check(['username', 'email']).custom(([username, email]) => {
+    return User.findOne({
+      where: {[Op.or]: {username, email}}
+    })
+    .then(user => {
+      if (user) {
+        if (user.username === username) {
+          return Promise.reject('The provided username is already in use.')
+
+        } else if (user.email === email) {
+          return Promise.reject('The provided email is already in use.')
+
+        }
+          
+      }
+    })
+
+  }) 
+]
 
 router.get('/login', csrfProtection, restoreUser, function(req, res) {
   if (res.locals && res.locals.authenticated) {
@@ -14,7 +73,7 @@ router.get('/login', csrfProtection, restoreUser, function(req, res) {
   res.render('login', { csrfToken: req.csrfToken() });
 });
 
-router.post('/login', csrfProtection, asyncHandler(async (req, res) => {
+router.post('/login', csrfProtection, validateEmailAndPass, asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({
@@ -57,7 +116,7 @@ router.get('/signup',csrfProtection,restoreUser, async function(req, res, next) 
   });
 });
 
-router.post('/signup',csrfProtection, async function(req,res){
+router.post('/signup',csrfProtection, validateEmailAndPass, signupValidator, async function(req,res){
   const {password,confirmPassword,email,username} = req.body
  
  try{
