82e3bba5130a750d12357e110a8e456c72ae6bdd
Kyle <kylevhuang@gmail.com>
2021-11-17 10:59:18 -0800

    comments

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
-var {User, Post} = require('../db/models');
+var {User, Post,Follow} = require('../db/models');
 const { loginUser, restoreUser } = require("../auth");
 /* GET home page. */
 router.get('/', restoreUser,async function(req, res, next) {
+  /*
+  To get users with an array of followers
+  const getUsers = await User.findAll({
+    include: {
+      model: User,
+      as:"Followers"
+    }
+  });
+  to get users with an array of following
+  const getUsers = await User.findAll({
+    include: {
+      model: User,
+      as:"Followers"
+    }
+  });
+  */
+  console.log(getUsers);
   const sixUsers = await Post.findAll({
     include: User,
     limit:6
