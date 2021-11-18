ee5bd01c6a71dbdc909b3a36f8c5a3b8690a87fa
Kyle <kylevhuang@gmail.com>
2021-11-17 20:32:32 -0800

    d

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -33,6 +33,7 @@ document.addEventListener("DOMContentLoaded",() => {
     if(background) observer.observe(background);
 
     //metabar menu click listener
+  if(document.querySelector('.metabar-menu')){
     document.querySelector('.metabar-menu').addEventListener('click',(e) => {
         const dropdown = document.querySelector('.dropdown').style.display
         if(dropdown === 'block'){
@@ -41,7 +42,6 @@ document.addEventListener("DOMContentLoaded",() => {
             document.querySelector('.dropdown').style.display = 'block';
         }
     });
-
     // close menu when clicking outside of it
     document.querySelector('.dropdown').addEventListener('click',(e) => {
         e.stopPropagation();
@@ -53,38 +53,39 @@ document.addEventListener("DOMContentLoaded",() => {
     /*
         navigation for following and recommended stories
     */
-   function changeActiveBtn(e,btnName){
-        if(e.target.classList.contains('snbOff')){
-            e.target.classList.remove('snbOff');
-            e.target.classList.add('snb');
-            document.querySelector(btnName).classList.remove('snb');
-            document.querySelector(btnName).classList.add('snbOff');
-        }
-   } 
-    document.querySelector('.snButton1').addEventListener('click',(e) => {
-        changeActiveBtn(e,'.snButton2');
-        document.querySelector('.reco-content').style.display = 'none';
-        document.querySelector('.follow-content').style.display = 'block';
-    });
-    document.querySelector('.snButton2').addEventListener('click',(e) => {
-        changeActiveBtn(e,'.snButton1');
-        document.querySelector('.reco-content').style.display = 'block';
-        document.querySelector('.follow-content').style.display = 'none';
-    });
+    function changeActiveBtn(e,btnName){
+            if(e.target.classList.contains('snbOff')){
+                e.target.classList.remove('snbOff');
+                e.target.classList.add('snb');
+                document.querySelector(btnName).classList.remove('snb');
+                document.querySelector(btnName).classList.add('snbOff');
+            }
+    } 
+        document.querySelector('.snButton1').addEventListener('click',(e) => {
+            changeActiveBtn(e,'.snButton2');
+            document.querySelector('.reco-content').style.display = 'none';
+            document.querySelector('.follow-content').style.display = 'block';
+        });
+        document.querySelector('.snButton2').addEventListener('click',(e) => {
+            changeActiveBtn(e,'.snButton1');
+            document.querySelector('.reco-content').style.display = 'block';
+            document.querySelector('.follow-content').style.display = 'none';
+        });
 
-    
-    document.querySelector('.browse').addEventListener('click',(e) => {
-        document.querySelector('.snButton1').classList.remove('snb');
-        document.querySelector('.snButton1').classList.add('snbOff');
-        document.querySelector('.snButton2').classList.remove('snbOff');
-        document.querySelector('.snButton2').classList.add('snb');
         
-        document.querySelector('.reco-content').style.display = 'block';
-        document.querySelector('.follow-content').style.display = 'none';
-    });
-    if(document.querySelector('.follow-content').children.length){
-        document.querySelector('.empty-follow').style.display = 'block';
-    }else{
-        document.querySelector('.empty-follow').style.display = 'none';
+        document.querySelector('.browse').addEventListener('click',(e) => {
+            document.querySelector('.snButton1').classList.remove('snb');
+            document.querySelector('.snButton1').classList.add('snbOff');
+            document.querySelector('.snButton2').classList.remove('snbOff');
+            document.querySelector('.snButton2').classList.add('snb');
+            
+            document.querySelector('.reco-content').style.display = 'block';
+            document.querySelector('.follow-content').style.display = 'none';
+        });
+        if(document.querySelector('.follow-content').children.length){
+            document.querySelector('.empty-follow').style.display = 'block';
+        }else{
+            document.querySelector('.empty-follow').style.display = 'none';
+        }
     }
-})
+});
