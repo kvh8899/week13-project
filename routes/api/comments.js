1cd16c028d176cf6f9a11ba133dd0bf59e5fbfcf
Ricky Thang <rthang11@yahoo.com>
2021-11-20 22:14:11 -0800

    Debugged routes/api/comments.js. likedComment now within scope of response

--- routes/api/comments.js
+++ routes/api/comments.js
@@ -22,11 +22,11 @@ router.post('/comments/:id/likes', restoreUser, asyncHandler(async(req, res, nex
                 userId: res.locals.user.id,
                 commentId: req.params.id
             })
-        }
 
         /* Set status 201, and
            respond with json, likedComment */
         res.status(201).json(likedComment);
+        };
     }));
 
 
