1b08985e898f606833a082875d89f6a114445fbf
Kyle <kylevhuang@gmail.com>
2021-11-20 19:51:30 -0800

    feat: users can now like and unlike comments

---
+++ routes/api/comments.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const {Post, User,Follow,PostLike,CommentLike} = require('../../db/models');
+const {getRandomInt,asyncHandler} = require('../utils.js')
+const {restoreUser} = require('../../auth.js');
+const createError = require("http-errors");
+const limit = 6;
+const { Op } = require("sequelize");
+const router = express.Router();
+
+
+//get a like from a comment
+router.get('/:commentId(\\d+)/likes', asyncHandler(async(req,res,next) => {
+    const likes = await CommentLike.findByPk(req.params.commentId);
+    res.json(likes);
+}));
+
+router.post('/:commentId(\\d+)/likes',restoreUser, asyncHandler(async(req,res,next) => {
+    if(!res.locals.authenticated){
+        next(createError(401));
+    }else{
+        const commentLike = 
+        await CommentLike.create({
+            userId:res.locals.user.id,
+            commentId:req.params.commentId
+        });
+        res.json(commentLike);
+    }
+}));
+
+router.delete('/likes/:commentId(\\d+)',restoreUser, asyncHandler(async(req,res,next) => {
+    
+    if(!res.locals.authenticated){
+        next(createError(401));
+    }else{
+        const commentDstry = await CommentLike.findOne({
+            where:{
+                id:req.params.commentId
+            }
+        });
+        commentDstry.destroy();
+        res.json({message:"Comment Like Successfully Deleted"});
+    }
+}));
+
+module.exports = router;
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const apiStories = require('./stories');
-
+const apiComments = require('./comments');
 
 const router = express.Router();
 
 router.use('/stories',apiStories);
-
-//router.use('/comments',)
+router.use('/comments',apiComments);
 
 module.exports = router;
